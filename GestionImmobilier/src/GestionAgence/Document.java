a1004f57cbc8903ebdca5992ea4281f6f22a178e
sebastian-andreas.si <sebastian-andreas.si@MI000-GS037-07.mathinfo.i-univ-tlse2.fr>
2017-12-14 16:21:48 +0100

    testing if i can push.

--- GestionImmobilier/src/GestionAgence/Document.java
+++ GestionImmobilier/src/GestionAgence/Document.java
@@ -1,5 +1,9 @@
 package GestionAgence;
 
 public class Document {
-
+	public enum filetype {/** passager assis à l'intérieur */  html, 
+        /** passager debout à l'intérieur */ css,  
+        /** passager à l'extérieur */        js};
+	private String contenu; //filepath
+	private filetype type;
 }
